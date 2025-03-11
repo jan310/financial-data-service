6da198855e898f7786d33e8e61b80812a93d4593
jan310 <jan310.ondra@gmail.com>
2025-03-11 20:10:44 +0100

    configured DB and Auth0 connection

---
+++ src/main/java/jan/ondra/financialdataservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package jan.ondra.financialdataservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .cors(withDefaults())
+            .build();
+    }
+
+}
